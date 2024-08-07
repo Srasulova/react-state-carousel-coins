2ce81051d3c1ad47b39eb39f64bc3cc4951dc287
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-08-07 04:03:24 -0400

    part 1 and part 2

--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -2,12 +2,20 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
-it("works when you click on the right arrow", function() {
+it("renders without crashing", function () {
+  render(<Carousel photos={TEST_IMAGES} title="images for testing" />);
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("works when you click on the right arrow", function () {
   const { container } = render(
-    <Carousel
-      photos={TEST_IMAGES}
-      title="images for testing"
-    />
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
   );
   // expect the first image to show, but not the second
   expect(
